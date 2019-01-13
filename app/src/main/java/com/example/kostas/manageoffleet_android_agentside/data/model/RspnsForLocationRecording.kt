b6d192bd6a530856b2037026cac23d79e2ab73fb
Michalis Chaviaras <michav1510@gmail.com>
2019-01-13 19:51:23 +0200

    Successfull change to the successfullLoginActivity

---
+++ app/src/main/java/com/example/kostas/manageoffleet_android_agentside/data/model/RspnsForLocationRecording.kt
@@ -0,0 +1,19 @@
+package com.example.kostas.manageoffleet_android_agentside.data.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class RspnsForLocationRecording {
+
+    @SerializedName("result")
+    @Expose
+    public var result: Int? = null
+
+
+    override fun toString(): String {
+        return "RspnsForAuthentication{" +
+                "result='" + result + '\''.toString() +
+                '}'.toString()
+    }
+
+}
--- app/src/main/java/com/example/kostas/manageoffleet_android_agentside/data/model/remote/APIService.kt
+++ app/src/main/java/com/example/kostas/manageoffleet_android_agentside/data/model/remote/APIService.kt
@@ -1,6 +1,7 @@
 package com.example.kostas.manageoffleet_android_agentside.data.model.remote
 
-import com.example.kostas.manageoffleet_android_agentside.data.model.Post
+import com.example.kostas.manageoffleet_android_agentside.data.model.RspnsForAuthentication
+import com.example.kostas.manageoffleet_android_agentside.data.model.RspnsForLocationRecording
 
 
 import org.json.JSONObject
@@ -11,11 +12,18 @@ import rx.Observable
 
 interface APIService {
 
-    @POST("send_location_info.php")
+    @POST("giveauthenticationtoagent.php")
     @Headers("Content-Type: application/json;charset=utf-8", "Accept: application/json;charset=utf-8", "Cache-Control: max-age=640000")
-    fun savePost(
+    fun sendCredntlsAndWaitForAuthentication(
 //            @Body jsonObject: String
             @Body jsonObject : JSONObject
-    ): Observable<Post>
+    ): Observable<RspnsForAuthentication>
+
+    @POST("recordlocationofagent.php")
+    @Headers("Content-Type: application/json;charset=utf-8", "Accept: application/json;charset=utf-8", "Cache-Control: max-age=640000")
+    fun sendLastLocationRecordingOfAgent(
+//            @Body jsonObject: String
+            @Body jsonObject : JSONObject
+    ): Observable<RspnsForLocationRecording>
 
 }
