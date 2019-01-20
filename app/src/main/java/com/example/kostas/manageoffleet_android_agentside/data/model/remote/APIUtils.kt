67d0687cf29d8a966c1f34261f7be3c243d1e6ee
Michalis Chaviaras <michav1510@gmail.com>
2019-01-20 15:25:03 +0200

    changes due to update of retrofit and change of server hosting

--- app/src/main/java/com/example/kostas/manageoffleet_android_agentside/data/model/remote/APIUtils.kt
+++ app/src/main/java/com/example/kostas/manageoffleet_android_agentside/data/model/remote/APIUtils.kt
@@ -4,7 +4,7 @@ package com.example.kostas.manageoffleet_android_agentside.data.model.remote
 
 object ApiUtils {
 
-    val BASE_URL = "http://havistudio.com/MonitoringOfPhones/"
+    val BASE_URL = "https://havistudio.com/MonitoringOfPhones/"
 
     val apiService: APIService
         get() = RetrofitClient.getClient(BASE_URL)!!.create(APIService::class.java)
